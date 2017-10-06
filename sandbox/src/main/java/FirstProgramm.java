75b7da2fc247b78b09994a50e273c6fc3109e26f
Elizaveta Karpova <elizaveta.karpova@teligent.ru>
2017-10-06 14:35:28 +0300

    acacac

--- sandbox/src/main/java/FirstProgramm.java
+++ sandbox/src/main/java/FirstProgramm.java
@@ -1,6 +1,6 @@
 public class FirstProgramm {
 
-public static void main (String[] args) {
-System.out.println("hey U bastard!");
-}
+    public static void main(String[] args) {
+        System.out.println("hey U bastard!");
+    }
 }
